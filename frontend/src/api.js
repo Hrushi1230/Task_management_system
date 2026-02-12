7843cc8a270669f1c6137d34a5a429bb1e5de1b5
hrushi1230 <hrushikeshbehera1230@gmail.com>
2026-02-12 16:28:39 +0530

    fix api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-system-wywy.onrender.com"
+  baseURL: "https://task-management-system-wywy.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
